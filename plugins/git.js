fb4bdc20a4990e261736504b9cc09d21796160f7
Salman dx <88703871+salman-dx@users.noreply.github.com>
2021-08-18 16:11:40 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,19 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+  r_text[0] = "https://h.top4top.io/p_2049zyydb0.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ameer Suhail*
-*owner number wa.me/919946432377*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater salman dx*
+*owner number wa.me/918129727300*
 *bot setting video : https://youtu.be/_D4ZYuUSXjs
-*githublink       https://github.com/ameer-kallumthodi/pikachu*
-*audio commads    https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*sticker commads  https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*githublink       https://github.com/salman-dx/lisa*
+*audio commads    https://github.com/salman-dx/lisa/tree/master/uploads*
+*sticker commads  https://github.com/salman-dx/lisa/tree/master/uploads*
 `}) 
 
 }));
